54b0abd362280ab2abd03969d497a996fde872da
Jan Tarnogórski <tarnogorskijan@gmail.com>
2023-03-27 17:59:58 +0200

    sdsdd

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-function deleteRow(row)
-{
+function deleteRow(row) {
     document.getElementById('Tabela').deleteRow(row);
 }
 
-function createRow(infobaner=false, sala=0, lekcja="TEST", klasa="SYSTEMU", uwagi="-")
-{
-    var x=document.getElementById('Tabela');
+function createRowNormal(sala = 0, lekcja = "TEST", klasa = "SYSTEMU", uwagi = "-") {
+    var x = document.getElementById('Tabela');
     var len = x.rows.length;
     // deep clone the targeted row
     row = x.insertRow(len)
-    if (infobaner) {
-        row.innerHTML = 
-        `<td colspan="4" class="info-scroll"><div class="scroll-left"><p><b>UWAGA:</b> TEST SYSTEMU - Informacje mogą być nie prawidłowe! </p></div></td>`;
-    } else {
-        row.innerHTML = 
+    row.innerHTML =
         `<td id="${len}S" class="sala">${sala}</td>
         <td id="${len}L" class="lekcje">${lekcja}</td>
         <td id="${len}K" class="klasa">${klasa}</td>
         <td id="${len}I" class="inne">${uwagi}</td>`;
+}
+
+function createRowInfo(uwaga=true, tresc='TEST SYSTEMU - Informacje mogą być nie prawidłowe!') {
+    var x = document.getElementById('Tabela');
+    var len = x.rows.length;
+    // deep clone the targeted row
+    row = x.insertRow(len)
+    if (uwaga == true) {
+        var uwagatext = '<b>UWAGA:</b>'
     }
+    row.innerHTML = 
+        `<td colspan="4" class="info-scroll"><div class="scroll-left">
+        <p>${uwagatext} ${tresc} </p>
+        </div></td>`;
 }
 
 var czas = 10 + (window.innerWidth / 400)
